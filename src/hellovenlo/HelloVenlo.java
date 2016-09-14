0445d84f42b09d51d68a15bc2e94eaac29026d5b
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-09-14 13:42:51 +0200

    initial tips and tricks

---
+++ src/hellovenlo/HelloVenlo.java
@@ -0,0 +1,33 @@
+package hellovenlo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloVenlo {
+
+  /*  
+    //Tips:
+    //1. Install Darcula plugin.
+    //1a. Ctrl-Shift-Enter: remove noisy windows around editor
+    //2. Under the Help menu is the Keyboard Shortcut card.
+    //3. Ctrl-Shift-Up/Down = copy up/down
+    //4. Alt-Shift-Up/Down = move up/down
+    //5. Ctrl-E = delete line
+    //6. Code templates.
+    //7. sout+TAB key = System.out.println("");
+    //8. soutv+TAB key = System.out.println("greeting = " + greeting);
+    //9. Ctrl+semicolon = add the semicolon from anywhere in the line
+    //10. Ctrl+shift+semicolon = add the semicolon from anywhere plus line break
+    //11. Make your own code templates: Tools | Options
+    //12. for+Ctrl-Space: for loop assistance
+    //13. psvm = public static void main
+    //14. Ctrl-Tab: switch between documents
+    //15. keyboard shortcuts zoom text in/out
+ */
+    
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
---
+++ src/hellovenlo/Main.java
@@ -0,0 +1,9 @@
+package hellovenlo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
